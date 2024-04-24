262eb2e57416ecaec9eaab66678177c27d22f564
Atsushi Oka <oka.ats@gmail.com>
2024-04-24 16:34:45 +0900

    readAll stdin, unescape and output to stdout

--- index.mjs
+++ index.mjs
@@ -1,3 +1,15 @@
 
-test
+async function readAll(stream) {
+  const chunks = [];
+  for await (const chunk of stream) chunks.push(chunk);
+  return Buffer.concat(chunks).toString('utf8');
+}
+
+const input = await readAll(process.stdin);
+
+const output = input.replace( /\\u([a-f0-9]+)\b/g, (s,s0)=>{
+  return String.fromCharCode( Number.parseInt( s0,16 ) );
+})
+
+process.stdout.write( output );
 
